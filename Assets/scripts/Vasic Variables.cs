07b49c5df79a84a330beb21cc9352849a273e224
PaReng2 <korreppsj@naver.com>
2025-03-20 17:36:56 +0900

    [Feat] 점프까지 구현

--- Assets/scripts/Vasic Variables.cs
+++ Assets/scripts/Vasic Variables.cs
@@ -9,28 +9,28 @@ public class VasicVariables : MonoBehaviour
     public string playName = "홍길동";
     public bool isAlive = true;
     public int Speed = 3;
-    
-
-
+    public int JumpForce = 5;
+    public Rigidbody rb;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-    if (gold > 50)
+        if (gold > 50)
         {
             Debug.Log("아이템을 구매할 수 있습니다");
         }
 
-    else if (gold == 40)
+        else if (gold == 40)
         {
             Debug.Log("골드 40원을 보유중입니다. 10 골드만큼만 더 일하면 포르쉐를 살 수 있어요!");
         }
 
-    else
+        else
         {
             Debug.Log("골드가 부족합니다");
         }
 
-    
+
     }
 
     // Update is called once per frame
@@ -39,25 +39,37 @@ public class VasicVariables : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             gold = gold++; //10씩 증가
-            Debug.Log("현재 골드 : " +  gold);
+            Debug.Log("현재 골드 : " + gold);
         }
 
-        if (Input.GetKey(KeyCode.W))
+        Move();
+
+    }
+
+    private void Move()
+    {
+        float MoveX = Input.GetAxis("Horizontal");
+        float MoveZ = Input.GetAxis("Vertical");
+
+        Vector3 Direction = new Vector3(MoveX, 0, MoveZ).normalized;
+
+        rb.MovePosition(transform.position + Direction * Speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * Speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Speed * Time.deltaTime);
-        }
+
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+
     }
 }
