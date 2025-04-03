6abbaeae1b9f0b84f2c07cedd05798217634f6cb
PaReng2 <korreppsj@naver.com>
2025-04-03 17:37:50 +0900

    [Feat] Ray 발사 및 카메라 회전

--- Assets/scripts/RaycastShooter.cs
+++ Assets/scripts/RaycastShooter.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class RaycastShooter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            ShootRay();
+        }
+
         
+    }
+
+    private void ShootRay()
+    {
+            Ray ray = new Ray(transform.position, transform.forward); //발사할 ray의 시작점, 방향지정(메인 카메라에서 마우스 커서 방향으로 발사)
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) //raycast의 반환형은 bool로, ray를 맞았다면 true로 반환
+            {
+                Destroy(hit.collider.gameObject);
+            }
     }
 }
--- Assets/scripts/TargetSpawner.cs
+++ Assets/scripts/TargetSpawner.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class TargetSpawner : MonoBehaviour
 {
-    public GameObject TargetPrefabs;
+    public GameObject TargetPrefabs;       //과녁 프리팹(옵젝)
     public float generateTime = 3f;
     private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
@@ -18,9 +18,9 @@ public class TargetSpawner : MonoBehaviour
     {
         timer -= Time.deltaTime;            //timer 변수에서 deltaTime을 빼서 지난 시간 측정
 
-        if (timer < 0)
+        if (timer <= 0)
         {
-            timer -= generateTime;          //timer에 재생성 시간을 입력함
+            timer = generateTime;          //timer에 재생성 시간을 입력함
 
             float xPosition = Random.Range(-10f, 10f);
             Vector3 newPosition = new Vector3(xPosition, 0, 0);
